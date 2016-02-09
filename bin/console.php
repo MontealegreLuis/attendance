13645862109e6c389eb55b1275ed01b6953727ae
Luis Montealegre <montealegreluis@gmail.com>
2016-02-08 23:32:20 -0600

    Add console application.

---
+++ bin/console.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->run();
