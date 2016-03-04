32e0b04015b27bb68aec25b77c232d0e56174b6a
Luis Montealegre <montealegreluis@gmail.com>
2016-03-04 13:31:03 -0600

    Update attendance list in HTML page.

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -1,9 +1,15 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-(function() {
+(function($) {
     var source = new EventSource('attendance.php');
+    var rowTemplate = '<tr><td>{{content}}</td></tr>';
     source.addEventListener('message', function(event) {
-        console.log(event.data);
+        var attendance = JSON.parse(event.data);
+        var row = rowTemplate.replace(
+            '{{content}}',
+            attendance.occurred_on + ' - ' + attendance.attendance_id.value
+        );
+        $('.js-students').append(row);
     });
-})();
+})(jQuery);
