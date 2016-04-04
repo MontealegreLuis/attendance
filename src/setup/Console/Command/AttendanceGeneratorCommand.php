7fc91991fac9404da018252517db460bab93d2c4
Luis Montealegre <montealegreluis@gmail.com>
2016-04-04 15:28:23 -0500

    Refactor command to show a user-friendly output.

--- src/setup/Console/Command/AttendanceGeneratorCommand.php
+++ src/setup/Console/Command/AttendanceGeneratorCommand.php
@@ -52,10 +52,10 @@ class AttendanceGeneratorCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $today = new DateTime('now');
-        $bootcamps = $this->currentBootcamps($today);
-        $absentStudents = $this->absentStudents($bootcamps);
-        $students = $this->selectRandomStudents($absentStudents);
+        $bootcamps = $this->currentBootcamps(new DateTime('now'));
+        $absentStudents = $this->absentStudentsIn($bootcamps);
+        $students = $this->chooseRandom($absentStudents);
+        $this->showAttendanceToBeGenerated($output, $students);
         $this->generateAttendances($students);
     }
 
@@ -80,12 +80,13 @@ class AttendanceGeneratorCommand extends Command
      * @param array $bootcamps
      * @return array
      */
-    protected function absentStudents(array $bootcamps)
+    protected function absentStudentsIn(array $bootcamps)
     {
         $builder = $this->connection->createQueryBuilder();
         $builder
             ->addSelect('b.bootcamp_id')
             ->addSelect('b.start_time')
+            ->addSelect('b.cohort_name')
             ->addSelect('s.student_id')
             ->addSelect('a.attendance_id')
             ->from('bootcamps', 'b')
@@ -111,18 +112,18 @@ class AttendanceGeneratorCommand extends Command
      * @param array $absentStudents
      * @return array
      */
-    protected function selectRandomStudents(array $absentStudents)
+    protected function chooseRandom(array $absentStudents)
     {
-        $studentsIds = [];
+        $studentsInformation = [];
         foreach ($absentStudents as $bootcampId => $students) {
             shuffle($students);
-            $studentsIds[$bootcampId] = array_slice(
+            $studentsInformation[$bootcampId] = array_slice(
                 $students,
                 0,
                 mt_rand(1, 5)
             );
         }
-        return $studentsIds;
+        return $studentsInformation;
     }
 
     /**
@@ -130,22 +131,50 @@ class AttendanceGeneratorCommand extends Command
      */
     protected function generateAttendances(array $randomStudents)
     {
-        foreach ($randomStudents as $bootcampId => $students) {
-            foreach ($students as $student) {
-                $startTime = DateTime::createFromFormat(
-                    'Y-m-d H:i:s',
-                    $student['start_time']
-                );
-                $minutes = mt_rand(-90, 90);
-                $startTime->modify("$minutes minutes");
-                $attendance = An::attendance()
-                    ->withId($this->attendances->nextAttendanceId()->value())
-                    ->withStudentId(StudentId::fromLiteral($student['student_id']))
-                    ->recordedAt($startTime)
-                    ->build()
-                ;
-                $this->attendances->add($attendance);
+        foreach ($randomStudents as $studentsInBootcamp) {
+            $this->attendanceForBootcamo($studentsInBootcamp);
+        }
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param array $students
+     */
+    protected function showAttendanceToBeGenerated(
+        OutputInterface $output,
+        array $students
+    ) {
+        foreach ($students as $studentsByBootcamp) {
+            foreach ($studentsByBootcamp as $student) {
+                $name = $student['cohort_name'];
+                break;
             }
+            $output->writeln(sprintf(
+                'Generating <info>%d</info> attendances for <info>%s</info>...',
+                count($studentsByBootcamp),
+                $name
+            ));
+        }
+    }
+
+    /**
+     * @param $students
+     */
+    protected function attendanceForBootcamo($students)
+    {
+        foreach ($students as $student) {
+            $startTime = DateTime::createFromFormat(
+                'Y-m-d H:i:s',
+                $student['start_time']
+            );
+            $minutes = mt_rand(-90, 90);
+            $startTime->modify("$minutes minutes");
+            $attendance = An::attendance()
+                ->withId($this->attendances->nextAttendanceId()->value())
+                ->withStudentId(StudentId::fromLiteral($student['student_id']))
+                ->recordedAt($startTime)
+                ->build();
+            $this->attendances->add($attendance);
         }
     }
 }
