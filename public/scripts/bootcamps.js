02d9e3c909fb209924ba3e3aff3cab27d5243433
Luis Montealegre <montealegreluis@gmail.com>
2016-04-07 09:06:38 -0500

    Move JS code from the template to a .js file.

---
+++ public/scripts/bootcamps.js
@@ -0,0 +1,9 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+(function () {
+    'use strict';
+
+    $('.time').timepicker({showMeridian: false});
+    $('.date').datepicker({format: 'yyyy-mm-dd'});
+})(jQuery);
