0860e1d787e4b13c1cbf74ca447aa1f6433032d7
Luis Montealegre <montealegreluis@gmail.com>
2016-04-20 12:27:55 -0500

    Seed the database optionally in the reset command.

--- src/setup/Console/Command/ResetDatabaseCommand.php
+++ src/setup/Console/Command/ResetDatabaseCommand.php
@@ -9,6 +9,7 @@ namespace Codeup\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ResetDatabaseCommand extends Command
@@ -17,7 +18,23 @@ class ResetDatabaseCommand extends Command
     {
         $this
             ->setName('codeup:db:reset')
-            ->setDescription('Recreates and seeds the database')
+            ->setDescription('Recreates the database')
+            ->setHelp(<<<HELP
+Recreates the database
+
+<info>bin/setup codeup:db:reset</info>
+
+It also seeds the database if the option <info>seed</info> is passed:
+
+<info>bin/setup codeup:db:reset -s</info>
+HELP
+            )
+            ->addOption(
+                'seed',
+                's',
+                InputOption::VALUE_NONE,
+                'Seed the database with fake information'
+            )
         ;
     }
 
@@ -30,11 +47,16 @@ class ResetDatabaseCommand extends Command
         $command->run(new ArrayInput(['command' => 'codeup:db:create']), $output);
 
         $command = $this->getApplication()->find('migrations:migrate');
-        $input = new ArrayInput(['command' => 'migrations:migrate']);
-        $input->setInteractive(false);
-        $command->run($input, $output);
+        $migrationInput = new ArrayInput(['command' => 'migrations:migrate']);
+        $migrationInput->setInteractive(false);
+        $command->run($migrationInput, $output);
 
-        $command = $this->getApplication()->find('codeup:db:seed');
-        $command->run(new ArrayInput(['command' => 'codeup:db:seed']), $output);
+        if ($input->getOption('seed')) {
+            $command = $this->getApplication()->find('codeup:db:seed');
+            $command->run(
+                new ArrayInput(['command' => 'codeup:db:seed']),
+                $output
+            );
+        }
     }
 }
